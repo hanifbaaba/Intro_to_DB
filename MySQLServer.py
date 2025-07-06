292e02a5d3d239105ab01611d5d03208e0989438
hanifubaaba <baabahanif88@gmail.com>
2025-07-06 23:38:30 +0100

    refactored mysqlserver

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,3 +1,4 @@
+
 import mysql.connector
 from mysql.connector import Error
 
@@ -14,13 +15,14 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
-        print(f"Error while connecting to MySQL: {e}")
+    except mysql.connector.Error as err:
+        print(f"Error while connecting to MySQL: {err}")
 
     finally:
         if 'connection' in locals() and connection.is_connected():
             cursor.close()
             connection.close()
+            print("MySQL connection is closed.")
 
 if __name__ == "__main__":
     create_database()
